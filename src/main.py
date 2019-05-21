5a9e1f8a6d7b69f658be352ef4992297e7c5bfb3
qzed <qzed@users.noreply.github.com>
2019-05-22 00:08:45 +0200

    Print some generated trajectories

--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 def main():
     world = gridworld.IcyGridWorld(5)
 
+    for _ in range(5):
+        print(gridworld.generate_trajectory(world, lambda s: 0, 0, [4, 9, 14, 19, 24]))
+
     ax = plt.figure().add_subplot(111)
     plotting.plot_transition_probabilities(world, ax, cmap='gray')
     plt.show()
