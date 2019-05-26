a5534f314359d10142c701213f85d608de06a72b
qzed <qzed@users.noreply.github.com>
2019-05-26 02:21:14 +0200

    Collect common plot style attributes

--- src/main.py
+++ src/main.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    cmap = 'gray'
-    border_style = {'color': 'red', 'linewidth': 0.5}
+    style = {
+        'cmap': 'gray',
+        'border': {'color': 'red', 'linewidth': 0.5},
+    }
 
     world = gridworld.GridWorld(5)
 
@@ -17,7 +19,7 @@ def main():
         print(gridworld.generate_trajectory(world, lambda s: 0, 0, [4, 9, 14, 19, 24]))
 
     ax = plt.figure().add_subplot(111)
-    plotting.plot_transition_probabilities(world, ax, cmap=cmap, border=border_style)
+    plotting.plot_transition_probabilities(world, ax, **style)
     plt.show()
 
     reward = np.zeros(world.n_states)
@@ -25,7 +27,7 @@ def main():
     value = gridworld.value_iteration(world.p_transition, reward, 0.8)
 
     ax = plt.figure().add_subplot(111)
-    plotting.plot_state_values(world, value, ax, cmap=cmap, border=border_style)
+    plotting.plot_state_values(world, value, ax, **style)
     plt.show()
 
 
