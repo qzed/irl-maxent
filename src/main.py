5de41a841c872da1b99f8cc04afc75249b9c5032
qzed <qzed@users.noreply.github.com>
2019-05-29 16:23:41 +0200

    Refactor import names

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import gridworld as gw
-import maxent
-import plot
+import maxent as me
+import plot as P
 import trajectory as T
 
 import numpy as np
@@ -17,7 +17,7 @@ def main():
     world = gw.IcyGridWorld(5)
 
     ax = plt.figure().add_subplot(111)
-    plot.plot_transition_probabilities(ax, world, **style)
+    P.plot_transition_probabilities(ax, world, **style)
     plt.show()
 
     reward = np.zeros(world.n_states)
@@ -29,48 +29,48 @@ def main():
     value = gw.value_iteration(world.p_transition, reward, 0.8)
     policy = gw.optimal_policy_from_value(world, value)
 
-    pla = maxent.local_action_probabilities(world.p_transition, [24], reward)
-    svf = maxent.expected_svf_from_policy(world.p_transition, initial, [24], pla)
+    pla = me.local_action_probabilities(world.p_transition, [24], reward)
+    svf = me.expected_svf_from_policy(world.p_transition, initial, [24], pla)
 
     ax = plt.figure().add_subplot(111)
-    plot.plot_stochastic_policy(ax, world, pla, **style)
+    P.plot_stochastic_policy(ax, world, pla, **style)
     plt.show()
 
     ax = plt.figure().add_subplot(111)
-    plot.plot_state_values(ax, world, svf, **style)
+    P.plot_state_values(ax, world, svf, **style)
     plt.show()
 
     ax = plt.figure().add_subplot(111)
-    plot.plot_state_values(ax, world, value, **style)
-    plot.plot_deterministic_policy(ax, world, policy)
+    P.plot_state_values(ax, world, value, **style)
+    P.plot_deterministic_policy(ax, world, policy)
     plt.show()
 
     policy = gw.stochastic_policy_from_value(world, value, w=lambda x: x**2)
 
     ax = plt.figure().add_subplot(111)
-    plot.plot_stochastic_policy(ax, world, policy, **style)
+    P.plot_stochastic_policy(ax, world, policy, **style)
 
     ts = [*T.generate_trajectories(200, world, T.stochastic_policy_adapter(policy), 0, [24])]
     for t in ts:
-        plot.plot_trajectory(ax, world, t, color='yellow', alpha=0.025)
+        P.plot_trajectory(ax, world, t, color='yellow', alpha=0.025)
 
     plt.show()
 
     features = gw.state_features(world)
-    f_expect = maxent.feature_expectation_from_trajectories(features, ts)
+    f_expect = me.feature_expectation_from_trajectories(features, ts)
 
     ax = plt.figure().add_subplot(111)
-    plot.plot_state_values(ax, world, f_expect, **style)
+    P.plot_state_values(ax, world, f_expect, **style)
     plt.show()
 
-    irl_reward = maxent.irl(world.p_transition, features, [24], ts, 20, 0.2)
+    irl_reward = me.irl(world.p_transition, features, [24], ts, 20, 0.2)
     value = gw.value_iteration(world.p_transition, reward, 0.8)
     ts = [*T.generate_trajectories(200, world, T.stochastic_policy_adapter(policy), 0, [24])]
 
     ax = plt.figure().add_subplot(111)
-    plot.plot_state_values(ax, world, irl_reward, **style)
+    P.plot_state_values(ax, world, irl_reward, **style)
     for t in ts:
-        plot.plot_trajectory(ax, world, t, color='yellow', alpha=0.025)
+        P.plot_trajectory(ax, world, t, color='yellow', alpha=0.025)
     plt.show()
 
 
