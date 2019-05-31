696263482b09291f1f81453a3642448e63d7f306
qzed <qzed@users.noreply.github.com>
2019-05-31 18:22:52 +0200

    Add documentation for solver module

--- src/solver.py
+++ src/solver.py
@@ -1,7 +1,30 @@
+"""
+Generic solver methods for Markov Decision Processes (MDPs) and methods for
+policy computations for GridWorld.
+"""
+
 import numpy as np
 
 
 def value_iteration(p, reward, discount, eps=1e-3):
+    """
+    Basic value-iteration algorithm to solve the given MDP.
+
+    Args:
+        p: The transition probabilities of the MDP as table
+            `[from: Integer, to: Integer, action: Integer] -> probability: Float`
+            specifying the probability of a transition from state `from` to
+            state `to` via action `action` to succeed.
+        reward: The reward signal per state as table
+            `[state: Integer] -> reward: Float`.
+        discount: The discount (gamma) applied during value-iteration.
+        eps: The threshold to be used as convergence criterion. Convergence
+            is assumed if the value-function changes less than the threshold
+            on all states in a single iteration.
+
+    Returns:
+        The value function as table `[state: Integer] -> value: Float`.
+    """
     n_states, _, n_actions = p.shape
     v = np.zeros(n_states)
 
@@ -30,6 +53,30 @@ def value_iteration(p, reward, discount, eps=1e-3):
 
 
 def stochastic_value_iteration(p, reward, discount, eps=1e-3):
+    """
+    A modified version of the value-iteration algorithm to solve the given MDP.
+
+    During iteration, this modified version computes the average over all
+    state-action values instead of choosing the maximum. The modification is
+    intended to give a better expectation of the value for an agent that
+    chooses sub-optimal actions. It is intended as an alternative to the
+    standard value-iteration for automated trajectory generation.
+
+    Args:
+        p: The transition probabilities of the MDP as table
+            `[from: Integer, to: Integer, action: Integer] -> probability: Float`
+            specifying the probability of a transition from state `from` to
+            state `to` via action `action` to succeed.
+        reward: The reward signal per state as table
+            `[state: Integer] -> reward: Float`.
+        discount: The discount (gamma) applied during value-iteration.
+        eps: The threshold to be used as convergence criterion. Convergence
+            is assumed if the value-function changes less than the threshold
+            on all states in a single iteration.
+
+    Returns:
+        The value function as table `[state: Integer] -> value: Float`.
+    """
     n_states, _, n_actions = p.shape
     v = np.zeros(n_states)
 
@@ -58,6 +105,19 @@ def stochastic_value_iteration(p, reward, discount, eps=1e-3):
 
 
 def optimal_policy_from_value(world, value):
+    """
+    Compute the optimal policy from the given value function.
+
+    Args:
+        world: The `GridWorld` instance for which the the policy should be
+            computed.
+        value: The value-function dictating the policy as table
+            `[state: Integer] -> value: Float`
+
+    Returns:
+        The optimal (deterministic) policy given the provided arguments as
+        table `[state: Integer] -> action: Integer`.
+    """
     policy = np.array([
         np.argmax([value[world.state_index_transition(s, a)] for a in range(world.n_actions)])
         for s in range(world.n_states)
@@ -67,11 +127,52 @@ def optimal_policy_from_value(world, value):
 
 
 def optimal_policy(world, reward, discount, eps=1e-3):
+    """
+    Compute the optimal policy using value-iteration
+
+    Args:
+        world: The `GridWorld` instance for which the the policy should be
+            computed.
+        reward: The reward signal per state as table
+            `[state: Integer] -> reward: Float`
+        discount: The discount (gamma) applied during value-iteration.
+        eps: The threshold to be used as convergence criterion. Convergence
+            is assumed if the value-function changes less than the threshold
+            on all states in a single iteration.
+
+    Returns:
+        The optimal (deterministic) policy given the provided arguments as
+        table `[state: Integer] -> action: Integer`.
+
+    See also:
+        - `value_iteration`
+        - `optimal_policy_from_value`
+    """
     value = value_iteration(world.p_transition, reward, discount, eps)
     return optimal_policy_from_value(world, value)
 
 
 def stochastic_policy_from_value(world, value, w=lambda x: x):
+    """
+    Compute a stochastic policy from the given value function.
+
+    Args:
+        world: The `GridWorld` instance for which the the policy should be
+            computed.
+        value: The value-function dictating the policy as table
+            `[state: Integer] -> value: Float`
+        w: A weighting function `(value: Float) -> value: Float` applied to
+            all state-action values before normalizing the results, which
+            are then used as probabilities. I.e. choosing `x -> x**2` here
+            will cause the preference of suboptimal actions to decrease
+            quadratically compared to the preference of the optimal action.
+
+    Returns:
+        The stochastic policy given the provided arguments as table
+        `[state: Integer, action: Integer] -> probability: Float`
+        describing a probability distribution p(action | state) of selecting
+        an action given a state.
+    """
     policy = np.array([
         np.array([w(value[world.state_index_transition(s, a)]) for a in range(world.n_actions)])
         for s in range(world.n_states)
