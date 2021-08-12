8416c0e9114e30a2309f4d85cf4473d2a7b6893f
Nemlekar <hsnemlekar@wpi.edu>
2021-08-12 09:02:17 -0700

    create figures dir

--- src/plot_figures.py
+++ src/plot_figures.py
@@ -3,9 +3,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-match_accuracy = np.loadtxt("match_new.csv")
-predict_accuracy = np.loadtxt("predict_new.csv")
-random_accuracy = np.loadtxt("random_new.csv")
+match_accuracy = np.loadtxt("results/match.csv")
+predict_accuracy = np.loadtxt("results/predict.csv")
+random_accuracy = np.loadtxt("results/random.csv")
 sns.set(style="darkgrid", context="talk")
 
 steps = range(1, len(match_accuracy)+1)
@@ -19,13 +19,4 @@ plt.xlabel("Action")
 plt.ylabel("Accuracy")
 plt.gcf().subplots_adjust(bottom=0.15)
 plt.legend(["proposed", "random"], loc=4)
-plt.savefig("results1.jpg")
-
-# plt.figure(figsize=(10, 5))
-# plt.bar(steps, predict_accuracy)
-# plt.ylim(0, 1)
-# plt.xticks(steps)
-# plt.xlabel("Action")
-# plt.ylabel("Accuracy")
-# plt.gcf().subplots_adjust(bottom=0.15)
-# plt.savefig("trajectory_prediction_fixed.jpg")
+plt.savefig("figures/results1.jpg")
